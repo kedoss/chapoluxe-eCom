39bd4ed7d693c31237bfe4f421b89e654c7fd6c0
kedoss <kedarndicunguye6@gmail.com>
2025-03-25 10:04:56 +0200

    ajoute, retire au panier

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const productsContainer = document.querySelector(".produits");
 //console.log(data);
 const dialog = document.querySelector("dialog");
 
+let produitsCarte = [];
+//initialiser le nombre de produit
+const nombreProduit = document.querySelector(".carte .nombre");
+nombreProduit.textContent = produitsCarte.length;
+
 //Loop entre différents éléments
 const afficherProduit = (produits)=> {
     produits.forEach((produit) => {
@@ -44,10 +49,19 @@ input.addEventListener("keyup", (e) => {
     }
 });
 
+const testerSiProduitExiste = (arr,produit)=>{
+    const el = arr.find(p=> p.id === produit.id)
+    return el ? true : false;
+};
+
 //Actions sur les produits
 const cartes = document.querySelectorAll(".carte-produit");
 cartes.forEach((produit) => {
     produit.addEventListener("click", () => {
+        const contenuDialog = document.querySelector(".dialog-menu");
+        //Effacer le contenu passé
+        contenuDialog && contenuDialog.remove();
+
         dialog.showModal();
         console.log(produit.dataset);
         const curentproduct = data.filter(p => p.id == produit.dataset.id)[0];
@@ -55,7 +69,81 @@ cartes.forEach((produit) => {
         section.classList.add("dialog-menu");
         section.innerHTML = generateDialogHTML(curentproduct);
         dialog.appendChild(section);
-        console.log(curentproduct);
+        //console.log(curentproduct);
+
+        //changer de couleur
+        const couleurs = document.querySelectorAll(".color.change");
+        //console.log(couleurs);
+        couleurs.forEach((couleur,key) =>{
+            couleur.addEventListener("click",()=>{
+                const backgroundImg = document.querySelector(".gauche");
+
+                //Switch
+                switch (key) {
+                    case 0:
+                        backgroundImg.style.background = "#ff6b58";
+                        break;
+                
+                    default:
+                        backgroundImg.style.background = "#5f69d5";
+                        break;
+                }
+            });
+        });
+
+        
+        console.log(testerSiProduitExiste(produitsCarte, curentproduct));
+
+        //Ajouter produit dans le panier
+        const ajouter = document.querySelector(".ajouter");
+        const qte = document.querySelector(".qte")
+        qte.textContent = 0;
+        ajouter.addEventListener("click",()=>{
+            if (testerSiProduitExiste(produitsCarte, curentproduct)) {
+                const btnText =
+                `
+                <div class="icon"><i class="fa-solid fa-plus"></i>
+                </div>
+                <p>Ajouter au panier</p>`;
+                ajouter.innerHTML = btnText;
+                ajouter.classList.remove("ajoute");
+                produitsCarte = produitsCarte.filter((p) => p.id !== curentproduct.id);
+                nombreProduit.textContent = produitsCarte.length;
+            }
+            
+            else {
+                produitsCarte.push(curentproduct);
+                ajouter.textContent = "Effacer du panier";
+                ajouter.classList.add("ajoute");
+                nombreProduit.textContent = produitsCarte.length;
+                qte.textContent = 1;
+            }
+            
+        });
+
+        //Tester si une fois le produit existe et afficher directement le message effacer
+        if (testerSiProduitExiste(produitsCarte,curentproduct)) {
+            ajouter.textContent = "Effacer du panier";
+            ajouter.classList.add("ajoute");
+        }
+
+        //Quantité
+        const reduireBtn = document.querySelector(".counter .fa-minus");
+        const incrementeBtn = document.querySelector(".counter .fa-plus");
+
+        let qteProduit = testerSiProduitExiste (produitsCarte, curentproduct) ? 1:0;
+        incrementeBtn.addEventListener("click",()=>{
+            qteProduit = qteProduit +1;
+            qte.textContent = qteProduit;
+        
+        });
+
+        //reduire
+        reduireBtn.addEventListener("click",()=>{
+            qteProduit = qteProduit -1;
+            qte.textContent = qteProduit;
+            console.log("test");
+        });
     });
 });
 console.log(cartes);
